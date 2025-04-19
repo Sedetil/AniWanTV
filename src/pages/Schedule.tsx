919bb4c09c8329720b9d6c68e34e4518077de568
Sedetil <xtzysweet@gmail.com>
2025-04-19 12:39:55 +0700

    Change Schedule Design

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -100,7 +100,7 @@ const Schedule = () => {
             </div>
 
             {isLoading ? (
-              <div className="grid grid-cols-6 gap-4">
+              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
                 {[...Array(12)].map((_, i) => (
                   <div key={i} className="animate-pulse">
                     <div className="bg-gray-200 dark:bg-gray-700 rounded-md aspect-[2/3] mb-2"></div>
